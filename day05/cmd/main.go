2b9f7fcfe7ce3c07c544385b41798cd9ae4fa235
Jarangutan <53702919+jarangutan@users.noreply.github.com>
2023-12-05 22:38:04 -0500

    shoutouts to HFX for being a major g

--- day05/cmd/main.go
+++ day05/cmd/main.go
@@ -17,6 +17,39 @@ Add offset to seed while going through each map
 Get a result
 */
 
+type Range struct {
+	dst int
+	src int
+	rng int
+}
+
+func readUntilEmpty(input *bufio.Scanner) []Range {
+	mapping := []Range{}
+
+	for i := 0; input.Scan(); i++ {
+		line := input.Text()
+
+		if line == "" {
+			input.Scan() // skip next label
+			break
+		}
+
+		fields := strings.Fields(line)
+		dst, _ := strconv.Atoi(fields[0])
+		src, _ := strconv.Atoi(fields[1])
+		rng, _ := strconv.Atoi(fields[2])
+
+		mapping = append(mapping, Range{
+			dst: dst,
+			src: src,
+			rng: rng,
+		})
+	}
+
+	return mapping
+}
+
+// Thank you HFX on The Primeagen discord for hinting me in the right direction <3
 func part1(input *bufio.Scanner) {
 	input.Scan()
 	in := input.Text()
@@ -33,30 +66,27 @@ func part1(input *bufio.Scanner) {
 	input.Scan()
 	input.Scan()
 
-	for i := 0; input.Scan(); i++ {
-		line := input.Text()
+	// Gather the mappings to their respective namespaces
+	// That way we can break early on the seed to avoid re-hitting a range
+	seedToSoil := readUntilEmpty(input)
+	soilToFertilizer := readUntilEmpty(input)
+	fertilizerToWater := readUntilEmpty(input)
+	waterToLight := readUntilEmpty(input)
+	lightToTemperature := readUntilEmpty(input)
+	temperatureToHumidity := readUntilEmpty(input)
+	humidityToLocation := readUntilEmpty(input)
 
-		// Skip new line and label
-		if line == "" {
-			input.Scan()
-			input.Scan()
-			continue
-		}
+	mappings := [][]Range{}
+	mappings = append(mappings, seedToSoil, soilToFertilizer, fertilizerToWater, waterToLight, lightToTemperature, temperatureToHumidity, humidityToLocation)
 
-		fields := strings.Fields(line)
-		dst, _ := strconv.Atoi(fields[0])
-		src, _ := strconv.Atoi(fields[1])
-		rng, _ := strconv.Atoi(fields[2])
-
-		// fmt.Println("fields", dst, src, rng)
-		// fmt.Println("offset", dst-src)
-
-		for i, seed := range seeds {
-			if seed >= src && seed <= src+rng {
-				offset := dst - src
-				fmt.Println(offset)
-				seeds[i] += offset
-				// fmt.Println("Seed after offset", seeds[i])
+	for i := range seeds {
+		for _, mapping := range mappings {
+			for _, rng := range mapping {
+				if seeds[i] >= rng.src && seeds[i] <= rng.src+rng.rng {
+					offset := rng.dst - rng.src
+					seeds[i] += offset
+					break
+				}
 			}
 		}
 	}
@@ -71,4 +101,7 @@ func main() {
 
 	// ---- Part 1 ---- //
 	part1(input)
+
+	// ---- part 2 ---- //
+	// No part 2 due to time constraints :-)
 }
